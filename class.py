ee3effc2380bf2d442e3f369dda9a3ab9757d647
Abdirahmanelyas <abdirahman.elyas@student.moringaschool.com>
2024-09-05 23:46:23 +0300

    done

---
+++ class.py
@@ -0,0 +1,13 @@
+class Dog:
+   def __init__(self, color, name) :
+       self.color=color
+       self.name=name
+fido=Dog("brown," "nelly")
+print(fido)
+          
+      
+       
+    
+
+
+    
